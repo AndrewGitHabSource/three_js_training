589d55f06a40652e07c706f49e1fb07d48c60f45
Andrey Nozdrenko <nozdrenko.mail@gmail.com>
2024-11-13 16:25:57 +0200

    init

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,10 @@
 import * as THREE from 'three';
 
-// Создание сцены
 const scene = new THREE.Scene();
-
-// Создание камеры
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// Создание рендерера
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
