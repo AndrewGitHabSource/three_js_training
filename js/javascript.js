8c3d977437c9c4b2beb4636d59757006cb20145c
Andrey Nozdrenko <nozdrenko.mail@gmail.com>
2024-11-17 18:25:08 +0200

    add satellite

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,15 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+
+const orbitalRadius = 1.25;
+let angle = 0;
+const textureMoonLoader = new THREE.TextureLoader();
+const textureMoon = textureMoonLoader.load('textures/moon.jpg');
+const materialMoon = new THREE.MeshBasicMaterial({ map: textureMoon });
+const satelliteGeometry = new THREE.SphereGeometry(0.2, 58, 58);
+const satellite = new THREE.Mesh(satelliteGeometry, materialMoon);
+
 const geometry = new THREE.SphereGeometry(1, 64, 64);
 const textureLoader = new THREE.TextureLoader();
 const earthTexture = textureLoader.load('textures/4k_earth_daymap.png');
@@ -18,17 +27,18 @@ const material = new THREE.MeshPhongMaterial({
 });
 const earthMesh = new THREE.Mesh(geometry, material);
 const ambientLight = new THREE.AmbientLight(0x222222);
-const pointLight = new THREE.PointLight(0xffffff, 1, 50);
+const pointLight = new THREE.PointLight(0xffffff, 8, 50);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.z = 2;
+camera.position.z = 3;
 
 scene.add(earthMesh);
 scene.add(ambientLight);
 camera.add(pointLight);
 scene.add(camera);
+scene.add(satellite);
 
 pointLight.castShadow = true;
 pointLight.shadow.mapSize.width = 1024;
@@ -43,8 +53,19 @@ controls.maxDistance = 10;
 const animate = () => {
     requestAnimationFrame(animate);
 
+    satellite.position.y = orbitalRadius * Math.cos(angle);
+    satellite.position.z = orbitalRadius * Math.sin(angle);
+    satellite.position.x = orbitalRadius * Math.cos(angle);
+    angle += 0.01;
+
     earthMesh.rotation.y += 0.01;
     controls.update();
     renderer.render(scene, camera);
 }
 animate();
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
