64414680bf1679b2502358cfca4eb8f92a7d7606
Andrey Nozdrenko <nozdrenko.mail@gmail.com>
2024-11-17 17:02:19 +0200

    add texture, add light

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -17,22 +18,33 @@ const material = new THREE.MeshPhongMaterial({
 });
 const earthMesh = new THREE.Mesh(geometry, material);
 const ambientLight = new THREE.AmbientLight(0x222222);
-const pointLight = new THREE.PointLight(0xffffff, 275);
+const pointLight = new THREE.PointLight(0xffffff, 1, 50);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.z = 5;
+camera.position.z = 2;
 
 scene.add(earthMesh);
 scene.add(ambientLight);
-pointLight.position.set(7, 4, 10);
-scene.add(pointLight);
+camera.add(pointLight);
+scene.add(camera);
+
+pointLight.castShadow = true;
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 2;
+controls.maxDistance = 10;
 
 const animate = () => {
     requestAnimationFrame(animate);
 
     earthMesh.rotation.y += 0.01;
-
+    controls.update();
     renderer.render(scene, camera);
 }
 animate();
