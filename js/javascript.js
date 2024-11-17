da830a308fc3473a61b325e97e4adb7554fbe787
Andrey Nozdrenko <nozdrenko.mail@gmail.com>
2024-11-17 16:36:06 +0200

    add texture, add light

--- js/javascript.js
+++ js/javascript.js
@@ -6,12 +6,12 @@ const renderer = new THREE.WebGLRenderer();
 const geometry = new THREE.SphereGeometry(1, 64, 64);
 const textureLoader = new THREE.TextureLoader();
 const earthTexture = textureLoader.load('textures/4k_earth_daymap.png');
-const bumpTexture = textureLoader.load('textures/earth_bump.png');
-const specularTexture = textureLoader.load('textures/4k_earth_heightmap.png');
+const bumpTexture = textureLoader.load('textures/4k_earth_heightmap.png');
+const specularTexture = textureLoader.load('textures/4k_earth_specular_map.png');
 const material = new THREE.MeshPhongMaterial({
     map: earthTexture,
     bumpMap: bumpTexture,
-    bumpScale: 0.05,
+    bumpScale: 5,
     specularMap: specularTexture,
     specular: new THREE.Color(0x333333),
 });
@@ -21,10 +21,6 @@ const pointLight = new THREE.PointLight(0xffffff, 275);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-earthTexture.wrapS = THREE.RepeatWrapping;
-earthTexture.wrapT = THREE.RepeatWrapping;
-earthTexture.flipY = false;
 camera.position.z = 5;
 
 scene.add(earthMesh);
